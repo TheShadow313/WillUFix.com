ecbee9ae9a9070db4f33df0f76d21431ace5eff3
TheShadow313 <mazehtheshadow313@gmail.com>
2024-07-09 15:26:24 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,139 @@
+(function ($) {
+    "use strict";
+    
+    // Header scroll class
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            $('#header').addClass('header-scrolled');
+        } else {
+            $('#header').removeClass('header-scrolled');
+        }
+    });
+    if ($(window).scrollTop() > 100) {
+        $('#header').addClass('header-scrolled');
+    }
+    
+    
+    // Mobile Navigation
+    if ($('#nav-menu-container').length) {
+        var $mobile_nav = $('#nav-menu-container').clone().prop({
+            id: 'mobile-nav'
+        });
+        
+        $mobile_nav.find('> ul').attr({
+            'class': '',
+            'id': ''
+        });
+        
+        $('body').append($mobile_nav);
+        $('body').prepend('<button type="button" id="mobile-nav-toggle"><i class="fa fa-bars"></i></button>');
+        $('body').append('<div id="mobile-body-overly"></div>');
+
+        $(document).on('click', '#mobile-nav-toggle', function (e) {
+            $('body').toggleClass('mobile-nav-active');
+            $('#mobile-nav-toggle i').toggleClass('fa-times fa-bars');
+            $('#mobile-body-overly').toggle();
+        });
+        
+        $(document).on('click', '#mobile-nav ul li a', function (e) {
+            $('body').toggleClass('mobile-nav-active');
+            $('#mobile-nav-toggle i').toggleClass('fa-times fa-bars');
+            $('#mobile-body-overly').toggle();
+        });
+    }
+
+    
+    // Smooth scrolling on the navbar links
+    $(".nav-menu a, #mobile-nav a").on('click', function (event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+            
+            $('html, body').animate({
+                scrollTop: $(this.hash).offset().top - 70
+            }, 1500, 'easeInOutExpo');
+            
+            if ($(this).parents('.nav-menu').length) {
+                $('.nav-menu .menu-active').removeClass('menu-active');
+                $(this).closest('li').addClass('menu-active');
+            }
+        }
+    });
+    
+    // Navigation active state on scroll
+    var elm = document.querySelector('.nav-menu');
+    var ms = new MenuSpy(elm,{
+        activeClass   : 'menu-active',
+        threshold     : 71,
+        enableLocationHash: false
+    });
+
+
+    // jQuery counterUp
+    $('#counters .number').counterUp({
+        delay: 10,
+        time: 1000
+    });
+
+
+    // Porfolio isotope and filter
+    var portfolioIsotope = $('.portfolio-container').isotope({
+        itemSelector: '.portfolio-item',
+        layoutMode: 'fitRows'
+    });
+    $('.portfolio-flters li').on('click', function () {
+        $(".portfolio-flters li").removeClass('filter-active');
+        $(this).addClass('filter-active');
+
+        portfolioIsotope.isotope({filter: $(this).data('filter')});
+    });
+
+
+    // Testimonial-carousel
+    $(".testimonial-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 2000,
+        center: false,
+        dots: false,
+        loop: true,
+        margin: 25,
+        nav : true,
+        navText : [
+            '<i class="bi bi-arrow-left"></i>',
+            '<i class="bi bi-arrow-right"></i>'
+        ],
+        responsiveClass: true,
+        responsive: {
+            0:{
+                items:1
+            },
+            576:{
+                items:1
+            },
+            768:{
+                items:2
+            },
+            992:{
+                items:2
+            },
+            1200:{
+                items:2
+            }
+        }
+    });
+    
+    
+    // Back to top button
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            $('.back-to-top').fadeIn('slow');
+        } else {
+            $('.back-to-top').fadeOut('slow');
+        }
+    });
+    $('.back-to-top').click(function () {
+        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+        return false;
+    });
+    
+})(jQuery);
+
